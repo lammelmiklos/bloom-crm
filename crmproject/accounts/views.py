ab1cbba712dbe72cc37ac660ae178a0d7cae43aa
Miklos Lammel <miklos.lammel@siemens.com>
2022-06-18 20:20:37 +0200

    Html templates added with Bootstrap, plus static file css was added

--- crmproject/accounts/views.py
+++ crmproject/accounts/views.py
@@ -4,11 +4,22 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('home')
-
+    return render(request, 'accounts/dashboard.html')
 
 def products(request):
-    return HttpResponse('products')
+    return render(request, 'accounts/products.html')
 
 def customer(request):
-    return HttpResponse('customer')
+    return render(request, 'accounts/customer.html')
+
+
+'''
+This is the template structure
+-- accounts
+---- templates
+------ accounts
+-------- dashboard.html
+-------- profile.html
+-------- customer.html
+
+'''
