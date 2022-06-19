209c4a60c82f0db1e7d5482ef64af94cb2afb986
Miklos Lammel <miklos.lammel@siemens.com>
2022-06-19 15:38:44 +0200

    More classes added to the model and registered with django admin

--- crmproject/accounts/models.py
+++ crmproject/accounts/models.py
@@ -2,8 +2,45 @@ from django.db import models
 
 # Create your models here.
 class Customer(models.Model):
-    name = models.CharField(max_length=200, null=True)
+    firstname = models.CharField(max_length=200, null=True)
+    lastname = models.CharField(max_length=200, null=True)
     phone = models.CharField(max_length=200, null=True)
     email = models.CharField(max_length=200, null=True)
     date_created = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self) -> str:
+        return f"{self.firstname} {self.lastname}"
+
+
+class Product(models.Model):
+    CATEGORY = (
+        ('Indoor', 'Indoor'),
+        ('Outdoor', 'Outdoor'), 
+    )
+
+    name = models.CharField(max_length=200, null=True)
+    price = models.FloatField(null=True)
+    category = models.CharField(max_length=200, null=True, choices=CATEGORY)
+    description = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f"{self.name}"
+
+
+class Order(models.Model):
+
+    STATUS = (
+        ('Pending', 'Pending'),
+        ('Out for delivery', ' Out for delivery'),
+        ('Delivered', 'Delivered'),
+
+    )
+
+    #customer = 
+    #product = 
+    date_created = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=200, null=True, choices=STATUS)
+
+    def __str__(self) -> str:
+        return f"Order status: {self.status}"
