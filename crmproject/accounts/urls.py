e3738cf4b3a13139eb5f6640512253ade911603c
Miklos Lammel <miklos.lammel@siemens.com>
2022-06-21 22:50:58 +0200

    Dynamic urls to dynamic links were added.

--- crmproject/accounts/urls.py
+++ crmproject/accounts/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home), # empty string is the base URL
-    path('products/', views.products), 
-    path('customer/<str:pk_customer>/', views.customer),
+    path('', views.home, name='home'), # empty string is the base URL
+    path('products/', views.products, name='products'), 
+    path('customer/<str:pk_customer>/', views.customer, name='customer'),
 ]
